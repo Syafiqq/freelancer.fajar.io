84664114bf069e4dc85ee7458a4241c19c18d468
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-11 16:57:49 +0700

    Add century view

---
+++ application/views/dashboard/century_admin.php
@@ -0,0 +1,306 @@
+<?php
+/**
+ * This <freelancer.fajar.io> project created by :
+ * Name         : syafiq
+ * Date / Time  : 10 December 2016, 10:42 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+if (!isset($isVersionEnabled))
+{
+    $isVersionEnabled = false;
+}
+
+if (!isset($dataCount))
+{
+    $dataCount = array();
+}
+
+?>
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Dashboard</title>
+    <meta name="description" content="">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" href="<?php echo base_url('/favicon-32x32.png') ?>" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo base_url('/favicon-16x16.png') ?>" sizes="16x16">
+    <link rel="manifest" href="<?php echo base_url('/manifest.json') ?>">
+    <link rel="mask-icon" href="<?php echo base_url('/safari-pinned-tab.svg') ?>" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/bootstrap/dist/css/bootstrap-theme.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/font-awesome/css/font-awesome.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/Ionicons/css/ionicons.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/AdminLTE/dist/css/AdminLTE.min.css') ?>">
+    <?php
+    if ($isVersionEnabled)
+    {
+        ?>
+        <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/AdminLTE/dist/css/skins/skin-purple.min.css') ?>">
+        <?php
+    }
+    else
+    {
+        ?>
+        <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/AdminLTE/dist/css/skins/skin-blue.min.css') ?>">
+        <?php
+    }
+    ?>
+
+    <script src="<?php echo base_url('assets/frontend/bower_components/initializr/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js') ?>"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="<?php echo base_url('assets/frontend/bower_components/AdminLTE/dist/js/html5shiv.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/frontend/bower_components/AdminLTE/dist/js/respond.min.js')?>"></script>
+    <![endif]-->
+</head>
+<?php
+if ($isVersionEnabled)
+{
+?>
+<body class="hold-transition skin-purple layout-top-nav">
+<?php
+}
+else
+{
+?>
+<body class="hold-transition skin-blue layout-top-nav">
+<?php
+}
+?>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an
+    <strong>outdated</strong>
+                          browser. Please
+    <a href="http://browsehappy.com/">upgrade your browser</a>
+                          to improve your experience.
+</p>
+<![endif]-->
+<div class="wrapper">
+
+    <header class="main-header">
+        <nav class="navbar navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="<?php echo site_url('dashboard') ?>" class="navbar-brand">
+                        <strong>Status</strong>
+                        Hukum
+                    </a>
+                </div>
+
+                <!-- Navbar Right Menu -->
+                <div class="navbar-custom-menu">
+                    <ul class="nav navbar-nav">
+                        <!-- User Account Menu -->
+                        <li class="dropdown user user-menu">
+                            <!-- Menu Toggle Button -->
+                            <a href="" class="dropdown-toggle" data-toggle="dropdown">
+                                <!-- The user image in the navbar-->
+                                <i class="fa fa-gears"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <?php if (!$isVersionEnabled)
+                                    {
+                                        ?>
+                                        <div class="pull-left">
+                                            <a id="versioning" href="<?php echo site_url('dashboard/do_versioning') ?>" class="btn btn-default btn-flat">Enable Editing</a>
+                                        </div>
+                                        <?php
+                                    } ?>
+                                    <div class="pull-right">
+                                        <a id="sign-out" href="<?php echo site_url('auth/do_signout') ?>" class="btn btn-default btn-flat">Sign out</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-custom-menu -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+    </header>
+    <!-- Full Width Column -->
+    <div class="content-wrapper">
+        <div class="container">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                    &nbsp;
+                </h1>
+                <ol class="breadcrumb">
+                    <li class="active">
+                        <a href="<?php echo site_url('dashboard') ?>">
+                            <i class="fa fa-dashboard"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                </ol>
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Tahun Undang Undang</h3>
+                    </div>
+                    <div class="box-body">
+                        <?php
+                        foreach ($dataCount as $idx => $val)
+                        {
+                            if ($idx % 10 == 0)
+                            {
+                                echo "<div class=\"row\" style='margin-top: 16px; margin-bottom: 16px'><div class=\"col-md-1\"></div><div class=\"col-md-1\"><button type=\"button\" " . ($val['count'] > 0 ? "action=\"" . site_url('dashboard/year?year=' . $val['year']) . "\"" : '') . " class=\"btn btn-block " . ($val['count'] <= 0 ? 'btn-danger disabled' : 'get-year btn-primary') . "\">{$val['year']}</button></div>";
+                            }
+                            else if ($idx % 10 == 9)
+                            {
+                                echo "<div class=\"col-md-1\"><button type=\"button\" " . ($val['count'] > 0 ? "action=\"" . site_url('dashboard/year?year=' . $val['year']) . "\"" : '') . " class=\"btn btn-block " . ($val['count'] <= 0 ? 'btn-danger disabled' : 'get-year btn-primary') . "\">{$val['year']}</button></div><div class=\"col-md-1\"></div></div>";
+                            }
+                            else
+                            {
+                                echo "<div class=\"col-md-1\"><button type=\"button\"  " . ($val['count'] > 0 ? "action=\"" . site_url('dashboard/year?year=' . $val['year']) . "\"" : '') . " class=\"btn btn-block " . ($val['count'] <= 0 ? 'btn-danger disabled' : 'get-year btn-primary') . "\">{$val['year']}</button></div>";
+                            }
+                        }
+                        ?>
+                    </div>
+                    <div class="box-footer">
+                        <ul class="pagination pull-right">
+                            <li class="page-item">
+                                <a class="page-link" href="<?php echo site_url('dashboard?century=1900') ?>">Tahun 1900</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="<?php echo site_url('dashboard?century=2000') ?>">Tahun 2000</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <div class="container">
+            <strong>Copyright &copy; Freelancer <?php echo isset($year) ? $year : 2016; ?>
+                <a href="<?php echo site_url('dashboard') ?>">
+                    <strong>Status</strong>
+                    Hukum
+                </a>
+                    .
+            </strong>
+            All rights reserved.
+        </div>
+        <!-- /.container -->
+    </footer>
+</div>
+<!-- ./wrapper -->
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<script>window.jQuery || document.write('<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/jquery/dist/jquery.min.js')?>"><\/script>')</script>
+
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/tether/dist/js/tether.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/remarkable-bootstrap-notify/dist/bootstrap-notify.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/fastclick/lib/fastclick.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/AdminLTE/dist/js/app.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/initializr/js/plugins.js') ?>"></script>
+<script type="text/javascript">
+    (function ($)
+    {
+        /*
+         Fullscreen background
+         */
+        $(function ()
+        {
+            $("a#sign-out, a#versioning").on('click', function (event)
+            {
+                event.preventDefault();
+                $.ajax({
+                    type: 'post',
+                    url: $(this).attr('href'),
+                    dataType: 'json',
+                    contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
+                })
+                    .done(function (data)
+                    {
+                        if (data.hasOwnProperty('data'))
+                        {
+                            if (data['data'].hasOwnProperty('notify'))
+                            {
+                                var notify = data['data']['notify'];
+                                for (var i = -1; ++i < notify.length;)
+                                {
+                                    $.notify({message: notify[i][0]}, {
+                                        type: notify[i][1],
+                                        template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+                                        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                                        '<span data-notify="icon"></span> ' +
+                                        '<span data-notify="title">{1}</span> ' +
+                                        '<span style="color: black" data-notify="message">{2}</span>' +
+                                        '<div class="progress" data-notify="progressbar">' +
+                                        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                                        '</div>' +
+                                        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                                        '</div>'
+                                    });
+                                }
+                            }
+                        }
+                        if (data.hasOwnProperty('code'))
+                        {
+                            if (data['code'] == 200)
+                            {
+                                setTimeout(function ()
+                                {
+                                    if (data.hasOwnProperty('redirect'))
+                                    {
+                                        location.href = data['redirect'];
+                                    }
+                                }, 2000);
+                            }
+                        }
+
+                    })
+                    .fail(function ()
+                    {
+                        $.notify({
+                            message: 'Error'
+                        }, {
+                            // settings
+                            type: 'danger'
+                        });
+                    })
+            });
+
+            $("button.get-year").on('click', function (event)
+            {
+                event.preventDefault();
+                location.href = $(this).attr('action');
+            });
+        });
+
+        /*
+         * Run right away
+         * */
+    })(jQuery);
+</script>
+</body>
+</html>
