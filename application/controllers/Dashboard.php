1318792dff169b69659918035f87ceebbae86f8d
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-17 18:35:10 +0700

    Completing all content layout

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -33,6 +33,7 @@ class Dashboard extends CI_Controller
         parent::__construct();
         $this->load->helper('url');
         $this->load->library('session');
+        $this->load->helper('cookie');
         // Your own constructor code
     }
 
@@ -43,12 +44,80 @@ class Dashboard extends CI_Controller
         $categories = $this->mdata->getCategoryCount();
         if (isset($_SESSION['user']['profile']))
         {
-            $this->load->view('dashboard/index_admin', array('year' => Carbon::now()->year, 'categories' => $categories));
+            $view = get_cookie('view_layout');
+            if (!empty($view))
+            {
+                switch ($view)
+                {
+                    case 'all' :
+                    {
+                        $content = $this->_getAllData();
+                        $layout = 'dashboard/index_admin_layout_all';
+                        $data = array('year' => Carbon::now()->year, 'data' => $content, 'meta' => array('domain' => base_url(), 'path' => '/dashboard'));
+                    }
+                        break;
+                    case 'search' :
+                    {
+                        $layout = 'dashboard/index_admin_layout_search';
+                        $data = array('year' => Carbon::now()->year, 'categories' => $categories, 'meta' => array('domain' => base_url(), 'path' => '/dashboard'));
+                    }
+                        break;
+                    default :
+                    {
+                        $layout = 'dashboard/index_admin_default';
+                        $data = array('year' => Carbon::now()->year, 'categories' => $categories, 'meta' => array('domain' => base_url(), 'path' => '/dashboard'));
+                    }
+                }
+            }
+            else
+            {
+                $layout = 'dashboard/index_admin_default';
+                $data = array('year' => Carbon::now()->year, 'categories' => $categories, 'meta' => array('domain' => base_url(), 'path' => '/dashboard'));
+            }
+            $this->load->view($layout, $data);
         }
         else
         {
             $this->load->view('dashboard/index_free', array('year' => Carbon::now()->year, 'categories' => $categories));
         }
+    }
+
+    private function _getAllData()
+    {
+        $this->load->model('mdata');
+        $this->load->model('mcategory');
+        $this->load->model('mtag');
+        $data = $this->mdata->getAllDataNo();
+        $_category = array();
+        $category = $this->mcategory->getAll();
+        foreach ($category as $value)
+        {
+            $_category["${value['id']}."] = $value;
+        }
+        unset($category);
+        $category = $_category;
+        unset($_category);
+        if (count($data) <= 0)
+        {
+            $data = array();
+        }
+        else
+        {
+            foreach ($data as $key => $value)
+            {
+                if ($value['tag'] <= 0)
+                {
+                    $data[$key]['tag'] = array();
+                }
+                else
+                {
+                    $data[$key]['tag'] = $this->mtag->getFromDataTag($value['id']);
+                }
+                $data[$key]['category'] =& $category["${value['category']}."];
+            }
+        }
+
+        return $data;
     }
 
 }
